bc8a5ca061764a7194cab440c3720755796d1878
Hazeglide <41839756+hazeglide@users.noreply.github.com>
2018-10-29 20:05:32 +0100

    add 'Supplement' text match

--- src/content.js
+++ src/content.js
@@ -3,7 +3,7 @@ var browser = (typeof browser === 'undefined' ? chrome : browser)
 function downloadAllBooks() {
   Array.prototype.slice.call(document.getElementsByClassName("js-gamelist-holder")[0].getElementsByTagName("a")).forEach(function (link) {
     console.log(link);
-    if (link.text.match(/EPUB|MOBI|PDF|PRC|Download/)) {
+    if (link.text.match(/EPUB|MOBI|PDF|PRC|Download|Supplement/)) {
       let url = link.href;
       browser.runtime.sendMessage({ text: url });
     }
