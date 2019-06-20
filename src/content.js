526d5b4cfdf1dc9430e8dded103b21400f69fe8c
Hazeglide <41839756+hazeglide@users.noreply.github.com>
2019-06-20 19:04:43 +0200

    added support for zip files

--- src/content.js
+++ src/content.js
@@ -3,7 +3,7 @@ var browser = (typeof browser === 'undefined' ? chrome : browser)
 function downloadAllBooks() {
   Array.prototype.slice.call(document.getElementsByClassName("js-gamelist-holder")[0].getElementsByTagName("a")).forEach(function (link) {
     console.log(link);
-    if (link.text.match(/EPUB|MOBI|PDF|PRC|Download|Supplement/)) {
+    if (link.text.match(/EPUB|MOBI|PDF|PRC|Download|Supplement|ZIP/)) {
       let url = link.href;
       browser.runtime.sendMessage({ text: url });
     }
